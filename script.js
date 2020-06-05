33f4027ec4e13141027e71d81d433be7a1e4c87b
Josh Cawthorne <joshcawthorne97@gmail.com>
2020-06-05 17:48:30 +0100

    Add on-scoll animations to skills

--- script.js
+++ script.js
@@ -1,10 +1,59 @@
-```
-$(document).scroll(function() {
+let hidden = true;
+let row2Pending = false;
+let row3Pending = false;
+let playRow2 = true;
+let playRow3 = true;
+
+$(document).ready(function () {
+  console.log("Script Loaded");
+});
+
+$(document).scroll(function () {
   var y = $(this).scrollTop();
-  if (y > 100) {
-    $('.rowport1').fadeIn();
+  console.log("Current Scroll Amount: ", y);
+  if (y > 600) {
+    if (hidden) {
+      hidden = false;
+      $(".skillItem").fadeIn();
+      row2Pending = true;
+      row3Pending = true;
+      playRow2 = true;
+      playRow3 = true;
+      setTimeout(() => {
+        if (playRow2) {
+          $(".skillItem2").fadeIn();
+        }
+      }, 150);
+      setTimeout(() => {
+        if (playRow3) {
+          $(".skillItem3").fadeIn();
+        }
+      }, 300);
+    }
   } else {
-    $('.rowport1')
+    if (!hidden) {
+      if (row2Pending) {
+        playRow2 = false;
+        row2Pending = false;
+      }
+      if (row3Pending) {
+        playRow3 = false;
+        row3Pending = false;
+      }
+      $(".skillItem").fadeOut();
+      $(".skillItem2").fadeOut();
+      $(".skillItem3").fadeOut();
+      hidden = true;
+    }
   }
 });
-```
+
+function testHide() {
+  if (hidden) {
+    $(".skillItem").fadeIn();
+    hidden = false;
+  } else {
+    $(".skillItem").fadeOut();
+    hidden = true;
+  }
+}
